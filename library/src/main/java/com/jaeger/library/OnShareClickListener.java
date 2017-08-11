05d5a777cca8ab1395da42e1a0620de973f2a0b2
xyb <328129250@qq.com>
2017-08-11 11:10:06 +0800

    commit

---
+++ library/src/main/java/com/jaeger/library/OnShareClickListener.java
@@ -0,0 +1,13 @@
+package com.jaeger.library;
+
+/**
+ * Created by 谢彦斌 on 2017/8/11.
+ * 修改时间:
+ * 版本号: 1.0.0
+ * 修改人:
+ */
+
+public interface OnShareClickListener {
+
+    void shareContent(String content);
+}
